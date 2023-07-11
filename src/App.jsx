cb098e9166690a9558ba355fecd4bd5b5e93a0c8
alexanderley <alexander.ley.inbox@gmail.com>
2023-07-11 11:30:49 +0200

    Lab done + Test done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 import { useState } from "react";
 import "./App.css";
 import foodsJson from "./foods.json";
-import FoodBox from "./components/FoodBoox";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
   const [foods, setFoods] = useState(foodsJson);
-  // console.log(foods);
 
-  function handleDeleteFood(index, foodKey) {
-    console.log(index);
-    const updatedFoods = foods.filter((food) => foodKey !== index);
+  function handleDeleteFood(indexToDelete) {
+    console.log(indexToDelete);
+    const updatedFoods = foods.filter((food, index) => index !== indexToDelete);
+    console.log(("updated Foods", updatedFoods));
     setFoods(updatedFoods);
 
     return updatedFoods;
   }
 
+  // function handleSubmit() {
+  //   event.preventDefault();
+  //   console.log("handle submit");
+  //   console.log(
+  //     "data:",
+  //     title.value,
+  //     image.value,
+  //     calories.value,
+  //     servings.value
+  //   );
+  //   const updatedFoods = foods.push({    title.value,
+  //     image.value,
+  //     calories.value,
+  //     servings.value})
+  // }
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h2>Add Food Entry</h2>
+      <AddFoodForm updateState={setFoods} />
+      <h1>Foodlist</h1>
       {foods.map((food, index) => {
         return (
           <FoodBox
             key={index}
             food={food}
-            onClick={() => handleDeleteFood(index, food.key)}
+            onClick={() => handleDeleteFood(index)}
+            style={{ border: "1px solid white" }}
           />
         );
       })}
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,68 @@
-// Your code here
+import React, { useState } from "react";
+
+export default function AddFoodForm(props) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState("");
+  const [servings, setServings] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("handle submit");
+    console.log("data:", name, image, calories, servings);
+
+    props.updateState((prevItems) => [
+      { name, image, calories, servings },
+      ...prevItems,
+    ]);
+  }
+
+  return (
+    <form
+      style={{ display: "flex", flexDirection: "column", gap: "15px" }}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name">name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="name"
+      />
+
+      <label htmlFor="image">Image</label>
+      <input
+        type="text"
+        id="image"
+        name="image"
+        value={image}
+        onChange={(event) => setImage(event.target.value)}
+        placeholder="Image"
+      />
+
+      <label htmlFor="calories">Calories</label>
+      <input
+        type="number"
+        id="calories"
+        name="calories"
+        value={calories}
+        onChange={(event) => setCalories(event.target.value)}
+        placeholder="Calories"
+      />
+
+      <label htmlFor="servings">Servings</label>
+      <input
+        type="number"
+        id="servings"
+        name="servings"
+        value={servings}
+        onChange={(event) => setServings(event.target.value)}
+        placeholder="Servings"
+      />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
