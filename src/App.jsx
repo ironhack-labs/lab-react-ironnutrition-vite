29b4ead43aff30485a6f3e3441a2163204cca4a0
Hassan Abou Saada <abousaada.hassan@gmail.com>
2023-07-13 00:43:13 +0200

    Lab + Ant Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React, { useState } from "react";
 import "./App.css";
+import FoodBox from "./components/FoodBox";
+import foodsJson from "./foods.json";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFoodItem = (foodId) => {
+    const updatedFoods = foods.filter((food) => food.id !== foodId);
+    setFoods(updatedFoods);
+  };
+
+  const addFoodItem = (newFoodItem) => {
+    setFoods([...foods, newFoodItem]);
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm addFoodItem={addFoodItem} />
+      <div>
+        {foods.map((food) => (
+          <FoodBox
+            key={food.name}
+            food={food}
+            deleteFoodItem={deleteFoodItem}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,28 @@
-// Your code here
+// components/FoodBox.jsx
+import React from "react";
+import { Card, Image, Typography, Button } from "antd";
+
+function FoodBox({ food, deleteFoodItem }) {
+  const { id, name, calories, image, servings } = food;
+
+  const handleDelete = () => {
+    deleteFoodItem(id);
+  };
+
+  return (
+    <Card className="FoodBox">
+      <Image src={image} alt={name} />
+      <Typography.Title level={4}>{name}</Typography.Title>
+      <Typography.Paragraph>Calories: {calories}</Typography.Paragraph>
+      <Typography.Paragraph>Servings: {servings}</Typography.Paragraph>
+      <Typography.Paragraph>
+        <b>Total Calories: {servings * calories} kcal</b>
+      </Typography.Paragraph>
+      <Button type="primary" danger onClick={handleDelete}>
+        Delete
+      </Button>
+    </Card>
+  );
+}
+
+export default FoodBox;
