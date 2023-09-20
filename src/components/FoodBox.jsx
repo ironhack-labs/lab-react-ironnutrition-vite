20cca3a32e9ac4c070380a2a6b365c16cfd5f6b4
Madhushree B <boylemadhushree@gmail.com>
2023-09-20 17:36:05 +0200

    Lab solved without bonus

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,26 @@
 // Your code here
+import React from "react"; //without this no react components will be displayed
+
+function FoodBox({ food, deleteFood }) {
+  // console.log(props);
+  return (
+    <div className="food-box">
+      <div>
+        <h2>{food.name}</h2>
+
+        <img src={food.image} />
+
+        <p>Calories: {food.calories}</p>
+        <p>Servings {food.servings}</p>
+
+        <p>
+          <b>Total Calories: {food.servings * food.calories} </b> kcal
+        </p>
+
+        <button onClick={() => deleteFood(food.id)}>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default FoodBox;
