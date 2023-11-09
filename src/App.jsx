d36c2bba910e96fdac705109d897538323e7c21c
Isabel Robleda <isabel.robleda@gmail.com>
2023-11-09 17:21:01 +0100

    first two iterations done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import Foodbox from "./components/FoodBox";
 
 function App() {
+
+  const [foodsJson, setFoodsJson] = useState("")
+
+  const addNewFood = (newFood) => {
+    const newList = [newFood, ...foodsJson];
+    setFoodsJson(newList);
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+  
+    <Foodbox food={ {
+      name: "Orange",
+      calories: 85,
+      image: "https://i.imgur.com/abKGOcv.jpg",
+      servings: 1
+    } } 
+    foodsArr={foodsJson} callbackToAdd={addNewFood}
+    
+    />
+
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,25 @@
-// Your code here
+import App from "../App";
+
+function Foodbox(props) {
+  
+    return (
+        <div>     
+            <p>{props.food.name}</p>
+
+            <img src={props.food.image} />
+
+            <p>Calories: {props.food.calories}</p>
+            <p>Servings {props.food.servings}</p>
+
+            <p>
+                <b>Total Calories: {props.food.servings * props.food.calories} </b> kcal
+            </p>
+
+            <button>Delete</button>
+
+            <App />
+      </div>
+    );
+  }
+  
+  export default Foodbox;
