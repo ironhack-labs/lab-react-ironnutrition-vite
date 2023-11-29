3913dbe63482a01153574bcd55af8f241e5001cd
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-11-29 20:34:42 +0100

    some refactor,and added id to new elemnts

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -6,47 +6,12 @@ import FoodEmpty from "./FoodEmpty";
 
 export default function FoodList() {
   const [foodArray, setFoodArray] = useState(foodsJson);
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-  const [calories, setCalories] = useState("");
-  const [servings, setServings] = useState("");
-
-  const handleName = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleImage = (e) => {
-    setImage(e.target.value);
-  };
-
-  const handleCalories = (e) => {
-    setCalories(e.target.value);
-  };
-
-  const handleServings = (e) => {
-    setServings(e.target.value);
-  };
-
   const handleDelete = (id) => {
     setFoodArray([...foodArray].filter((food) => food.id !== id));
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newFood = { name, calories, image, servings };
-    console.log(name, calories, image, servings);
-    console.log("selam ya");
-    setFoodArray([newFood, ...foodArray]);
-  };
   return (
     <div>
-      <AddFoodForm
-        handleName={handleName}
-        handleCalories={handleCalories}
-        handleImage={handleImage}
-        handleServings={handleServings}
-        handleSubmit={handleSubmit}
-      />
+      <AddFoodForm setFoodArray={setFoodArray} foodArray={foodArray} />
       <FoodBox foodArray={foodArray} handleDelete={handleDelete} />
       {!foodArray.length && <FoodEmpty />}
     </div>
