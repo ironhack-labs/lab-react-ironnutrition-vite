64e17f29c2311dce7d83952dcc4d0693e545babb
Ian IJmkers <ianymkers@gmail.com>
2023-12-07 21:38:41 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState([foodsJson])
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
