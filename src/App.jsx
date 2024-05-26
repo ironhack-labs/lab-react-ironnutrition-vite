68aa5d47de16946c03cd4606e872612a8e35ca3a
Nil E <nil.erdenn@gmail.com>
2024-05-26 19:49:24 +0200

    iteration 4 is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import FoodBox from "./components/FoodBox";
 
 function App() {
   const [foodList, setFoodList] = useState(foodsJson);
-
+  const handleDelete = (foodId) => {
+    const newList = [...foodList];
+    const indexToDelete = newList.findIndex((item) => {
+      return item.id === foodId;
+    });
+    newList.splice(indexToDelete, 1);
+    setFoodList(newList);
+  };
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
       {foodList.map((dish) => {
         return (
-          <FoodBox food={dish} key={dish.name}/>
-        )
+            <FoodBox
+            food={dish}
+            onDelete={handleDelete}
+            key={dish.id}
+          />
+        );
       })}
     </div>
   );
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,18 +1,19 @@
 import propTypes from "prop-types";
 import "./FoodBox.css";
 
-function FoodBox({ food }) {
-  const { name, image, calories, servings } = food;
+function FoodBox({ food,onDelete }) {
+  const { id, name, image, calories, servings } = food;
+
   return (
     <div>
       <p>{name}</p>
-      <img src={image} />
+      <img src={image} alt={name}/>
       <p>Calories: {calories}</p>
       <p>Servings {servings}</p>
       <p>
         <b>Total Calories: {servings * calories} </b> kcal
       </p>
-      <button>Delete</button>
+      <button onClick={() => {onDelete(id)}}>Delete</button>
     </div>
   );
 }
