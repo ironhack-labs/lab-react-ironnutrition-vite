879a4d3ba3df33d7a3475493af08ae7ea9377ef8
Daniela Pinheiro <daniela.plp@gmail.com>
2024-05-28 17:37:59 +0100

    labnutritiondone

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,67 @@
-// Your code here
+import { useState } from 'react'
+
+const AddFoodForm = ({ addFoodHandler }) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleImage = (event) => {
+    setImage(event.target.value);
+  };
+
+  const handleCalories = (event) => {
+    setCalories(event.target.value);
+  };
+
+  const handleServings = (event) => {
+    setServings(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newFood = {
+      id: Date.now(), // para adicionar um id único
+      name,
+      image,
+      calories: Number(calories),
+      servings: Number(servings),
+    };
+
+    addFoodHandler(newFood);
+
+    // Para limpar o formulário depois do usuário preencher e submeter
+    setName("");
+    setImage("");
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <div>
+      <h2>Add Food Entry</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input type="text" name="name" value={name} onChange={handleName} />
+        
+        <label>Image</label>
+        <input type="text" name="image" value={image} onChange={handleImage} />
+
+        <label>Calories</label>
+        <input type="number" name="calories" value={calories} onChange={handleCalories} />
+
+        <label>Servings</label>
+        <input type="number" name="servings" value={servings} onChange={handleServings} />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
 // Your code here
+import AddFoodForm from "./AddFoodForm";
+
+const FoodBox = ({food, deleteHandler}) => {
+    return (
+        <div>
+  <p>{food.name}</p>
+
+  <img src={food.image} alt={food.name} height={300}/>
+
+  <p>Calories: {food.calories}</p>
+  <p>Servings {food.servings}</p>
+
+  <p>
+    <b>Total Calories: {food.servings * food.calories} </b> kcal
+  </p>
+
+  <button onClick={() => deleteHandler(food.id)}>Delete</button>
+</div>
+    )
+
+}
+
+export default FoodBox;
