72d43d306142967c8506c3dbef57596f15fb3e1b
Bruno-Zamperoni <bzamperoni@gmai.com>
2024-07-13 05:46:58 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,60 @@
-import "./App.css";
+import foodsData from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsData);
+  const [foodname, setFoodname] = useState("");
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+  const [image, setImage] = useState("");
+
+  const handleName = (e) => {
+    setFoodname(e.target.value);
+  };
+  const handleCalories = (e) => {
+    setCalories(e.target.value);
+  };
+  const handleServings = (e) => {
+    setServings(e.target.value);
+  };
+  const handleImage = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newFood = {
+      name: foodname,
+      calories: calories,
+      servings: servings,
+      image: image,
+    };
+    setFoods([...foods, newFood]);
+    setFoodname("");
+    setCalories(0);
+    setServings(0);
+    setImage("");
+  }
+
+  const deleteFood = (name) => {
+    const foodToDelete = foods.filter((food) => {
+      return food.name !== name;
+    });
+    setFoods(foodToDelete);
+  };
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-white text-center mb-5">
+        Iron Nutrition
+      </h1>
+      <AddFoodForm handleName={handleName} handleCalories={handleCalories} handleServings={handleServings} handleImage={handleImage} handleSubmit={handleSubmit} foodname={foodname} calories={calories} servings={servings} image={image} />
+      <div className="flex flex-wrap justify-center gap-4 mt-5">
+        {foods.map((food, index) => {
+          return <FoodBox key={index} food={food} deleteFood={deleteFood} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,17 @@
-// Your code here
+import { Button } from "flowbite-react"
+
+export default function AddFoodForm({foodname, calories, servings, image, handleName, handleCalories, handleServings, handleImage, handleSubmit}) { 
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Name" onChange={handleName} value={foodname} />
+        <label htmlFor="calories">Calories</label>
+        <input type="number" id="calories" name="calories" placeholder="Calories" onChange={handleCalories} value={calories} />
+        <label htmlFor="servings">Servings</label>
+        <input type="number" id="servings" name="servings" placeholder="Servings" onChange={handleServings} value={servings} />
+        <label htmlFor="image">Image</label>
+        <input type="text" id="image" name="image" placeholder="Image" onChange={handleImage} value={image} />
+        <Button className="mt-5" type="submit">Create new food</Button>
+    </form>
+  )
+}
