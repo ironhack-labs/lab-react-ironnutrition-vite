4cc49f9283bb5aab252e9505cc16492c0bc696cc
elpezpr <gabrielpena@hotmail.com>
2024-07-25 14:30:48 -0400

    added #1 and #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState("")
+  const mockFood = {
+    name: "Orange",
+    calories: 85,
+    image: "https://i.imgur.com/abKGOcv.jpg",
+    servings: 1
+};
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-    </div>
+            <h1>LAB | React IronNutrition</h1>
+            <FoodBox food={mockFood} />
+        </div>
   );
 }
 
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
-// Your code here
+import React from "react";
+
+function FoodBox(props) {
+  const { food } = props;
+  return (
+    <div>
+      <p>{food.name}</p>
+
+      <img src={food.image} alt={food.name} />
+
+      <p>Calories: {food.calories}</p>
+      <p>Servings: {food.servings}</p>
+
+      <p>
+      <b>Total Calories: {food.servings * food.calories} kcal</b>
+      </p>
+
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;
