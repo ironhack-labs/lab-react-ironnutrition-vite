9733961320985c21475f65a43565b9f087b41757
Gonçalo Duarte <goncalopduarte23@gmail.com>
2024-08-05 15:37:13 +0100

    unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import React, { useState } from "react";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  function removeFood(name) {
+    setFoods(foods.filter((food) => food.name !== name));
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <FoodBox key={food.name} food={food} onRemove={removeFood} />
     </div>
   );
 }
