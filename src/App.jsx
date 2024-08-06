d3ae84c0f9d3ee1123fc2884d45fa5d8100264c0
rr1583 <rr1583@gmail.com>
2024-08-06 03:52:18 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import "./App.css";
+import React, { useState } from 'react';
+import foodsJson from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood = (foodName) => {
+    const updatedFoods = foods.filter(food => food.name !== foodName);
+    setFoods(updatedFoods);
+  };
+
+  const addFood = (newFood) => {
+    setFoods([...foods, newFood]);
+  };
+
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <AddFoodForm onAddFood={addFood} />
+      {foods.map((food, index) => (
+        <FoodBox key={index} food={food} onDelete={deleteFood} />
+      ))}
     </div>
   );
 }
