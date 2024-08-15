90d9167cbec3d1676c189cf509ffd5534451dbce
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-08-15 21:30:09 +0200

    finish the lab

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,58 @@
-// Your code here
+import { useState } from "react";
+import { Divider, Input } from "antd";
+
+function AddFoodForm({ createFoodItem }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleSubmission = (e) => {
+    e.preventDefault();
+    createFoodItem(name, calories, image, servings);
+    setName("");
+    setImage("");
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <form onSubmit={handleSubmission}>
+      <Divider>Name:</Divider>
+      <Input
+        type="text"
+        id="name"
+        name="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Divider>Image:</Divider>
+      <Input
+        type="text"
+        id="image"
+        name="image"
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <Divider>Calories:</Divider>
+      <Input
+        type="number"
+        id="calories"
+        name="calories"
+        onChange={(e) => setCalories(e.target.value)}
+      />
+
+      <Divider>Servings:</Divider>
+      <Input
+        type="number"
+        id="servings"
+        name="servings"
+        onChange={(e) => setServings(e.target.value)}
+      />
+
+      <Input type="submit" value="Create" />
+    </form>
+  );
+}
+
+export default AddFoodForm;
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,24 @@
-// Your code here
+import { Card, Button } from "antd";
+
+function FoodBox({ food, deleteFoodItem }) {
+  return (
+    <div>
+      <Card>
+        <p>{food.name}</p>
+
+        <img src={food.image} width="80vw" />
+
+        <p>Calories: {food.calories}</p>
+        <p>Servings {food.servings}</p>
+
+        <p>
+          <b>Total Calories: {food.calories * food.servings} </b> kcal
+        </p>
+
+        <Button onClick={() => deleteFoodItem(food.id)}>Delete</Button>
+      </Card>
+    </div>
+  );
+}
+
+export default FoodBox;
