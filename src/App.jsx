55133e9d08f181a0e893cec91d6d7b6457d8e8fa
Mr Footwork <pandau.ting@gmail.com>
2024-10-27 11:46:03 +0100

    iteration 4: delete list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css';
 import foodsJson from './foods.json';
 import FoodBox from './components/FoodBox';
+import { useState } from 'react';
 
 const state = {
 	foods: foodsJson,
 };
 
 function App() {
+	const [foods, setFoods] = useState(state.foods);
+
+	const deleteFood = idOfFoodToDelete =>
+		setFoods(prevFoods =>
+			prevFoods.filter(food => food.id !== idOfFoodToDelete)
+		);
+
 	return (
 		<div className='App'>
 			<h1>LAB | React IronNutrition</h1>
 
 			<ul style={{ listStyle: 'none' }}>
-				{state.foods.map((food, i) => (
-					<li key={i}>
-						<FoodBox food={food}></FoodBox>
+				{foods.map(food => (
+					<li key={food.id}>
+						<FoodBox food={food} onDelete={deleteFood}></FoodBox>
 					</li>
 				))}
 			</ul>
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,11 +1,13 @@
-function FoodBox({ food }) {
+function FoodBox({ food, onDelete }) {
 	const { name, image, calories, servings } = food;
+
+	const handleDelete = () => onDelete(food.id);
 
 	return (
 		<div style={{ border: '1px solid lightgrey', padding: '2rem' }}>
 			<h3>{name}</h3>
 
-			<img src={image} style={{ maxWidth: '40vw' }} />
+			<img src={image} style={{ maxWidth: '30vw' }} />
 
 			<p>Calories: {calories}</p>
 			<p>Servings {servings} </p>
@@ -14,7 +16,7 @@ function FoodBox({ food }) {
 				<b>Total Calories: {servings * calories} </b> kcal
 			</p>
 
-			<button>Delete</button>
+			<button onClick={handleDelete}>Delete</button>
 		</div>
 	);
 }
