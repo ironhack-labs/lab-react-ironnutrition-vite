1472abb46e4d6f1b50034f61463db12214ed91d2
Mr Footwork <pandau.ting@gmail.com>
2024-10-27 11:05:23 +0100

    iteration 2: FoodBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import foodsJson from './foods.json';
+import FoodBox from './components/FoodBox';
 
 const state = foodsJson;
 
@@ -7,7 +8,8 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>LAB | React IronNutrition</h1>
-			<pre style={{ textAlign: 'left' }}>{JSON.stringify(state, null, 2)}</pre>
+			{/* <pre style={{ textAlign: 'left' }}>{JSON.stringify(state, null, 2)}</pre> */}
+			<FoodBox food={state[10]}></FoodBox>
 		</div>
 	);
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+function FoodBox({ food }) {
+	const { name, image, calories, servings } = food;
+
+	return (
+		<div>
+			<h3>{name}</h3>
+
+			<img src={image} style={{ maxWidth: '40vw' }} />
+
+			<p>Calories: {calories}</p>
+			<p>Servings {servings} </p>
+
+			<p>
+				<b>Total Calories: {servings * calories} </b> kcal
+			</p>
+
+			<button>Delete</button>
+		</div>
+	);
+}
+
+export default FoodBox;
