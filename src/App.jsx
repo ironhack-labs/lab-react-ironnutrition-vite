eec2e6d73a454fc611c81da5349f26bf884c7e68
Mr Footwork <pandau.ting@gmail.com>
2024-10-27 10:32:24 +0100

    iteration 1: store data in the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
+import './App.css';
+import foodsJson from './foods.json';
+
+const state = foodsJson;
 
 function App() {
-  return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>LAB | React IronNutrition</h1>
+			<pre style={{ textAlign: 'left' }}>{JSON.stringify(state, null, 2)}</pre>
+		</div>
+	);
 }
 
 export default App;
