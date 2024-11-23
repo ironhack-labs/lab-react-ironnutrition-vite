8549e63b890e3b973b6f726c05af090ef5cec61e
Ignacio Molina <molinanacho33@gmail.com>
2024-11-23 11:32:57 +0100

    [Chore] Finish lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);  
+
+  const onAddFood = (newFood) => {
+    setFoods([...foods, newFood]);
+  };
+  const onDeleteFood = (foodId) => setFoods(foods.filter((food) => food.id !== foodId));
+
+  const foodBoxes = foods.map((food) => <FoodBox key = {food.id} food = {food} onDeleteFood = {onDeleteFood}/>);
+
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      
+      <AddFoodForm onAddFood = {onAddFood}/>
+
+      {foodBoxes}
+
     </div>
   );
 }
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,49 @@
-// Your code here
+import { useState } from "react";
+
+const initialData =
+    {
+        name: '',
+        image: '',
+        calories: 0,
+        servings: 0
+    }
+
+function AddFoodForm({onAddFood}) {
+    const [data, setData] = useState(initialData);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onAddFood({...data, id: crypto.randomUUID()});
+
+        setData(initialData);
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input name="name" id="name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
+            </div>
+
+            <div>
+                <label htmlFor="image">Image</label>
+                <input name="image" type="text" id="image" value={data.image} onChange={(e) => setData({...data, image: e.target.value})}/>
+            </div>
+
+            <div>
+                <label htmlFor="calories">Calories</label>
+                <input name="calories" type="number" min="0" id="calories" value={data.calories} onChange={(e) => setData({...data, calories: e.target.value})}/>
+            </div>
+
+            <div>
+                <label htmlFor="servings">Servings</label>
+                <input name="servings" type="number" min="0" id="servings" value={data.servings} onChange={(e) => setData({...data, servings: e.target.value})}/>
+            </div>
+
+            <button type="submit">Create</button>
+
+        </form>
+    )
+}
+
+export default AddFoodForm;
