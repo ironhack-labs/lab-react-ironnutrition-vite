88a73b9b2c06309920b8035ad8f2bfd70d1bc490
MMARTID <miguelmartindelgado1998@gmail.com>
2025-01-05 00:47:36 +0100

    iterations 1-4, render food and delete function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
+import { useState } from "react";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [food, setFood] = useState(foodsJson);
+  console.log("hi", food)
+  const deleteFood = (foodName) => {
+    const updatedFood = food.filter((eachFood) => eachFood.name !== foodName)
+    setFood(updatedFood)
+  }
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+
+        {food.map((eachFood) => {
+          return (
+            <div key={eachFood.name + eachFood.image}>
+              <FoodBox
+                deleteFood={deleteFood}
+                food={{
+                  name: `${eachFood.name}`,
+                  calories: `${eachFood.calories}`,
+                  image: `${eachFood.image}`,
+                  servings: `${eachFood.servings}`,
+                }}
+              />
+            </div>
+          );
+        })}
+
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,34 @@
-// Your code here
+function FoodBox({ food, deleteFood }) {
+  const totalCalories = food.servings * food.calories;
+
+  const containerStyle = {
+    border: "1px solid grey",
+    borderRadius: "12px",
+    padding: "20px",
+    maxWidth: "300px",
+    margin: "20px",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    textAlign: "center",
+    backgroundColor: "grey",
+  };
+  const imageStyle = {
+    width: "100%",
+    height: "auto",
+    borderRadius: "8px",
+    marginBottom: "15px",
+    backgroundColor:'white' //evitamos que las imagenes png salgan sin fondo 😉
+  };
+
+  return (
+    <div style={containerStyle}>
+      <p>{food.name}</p>
+      <img src={food.image} style={imageStyle}  />
+      <p>Calories: {food.calories}</p>
+      <p>Servings {food.servings}</p>
+      <p>
+      <b>Total Calories: {totalCalories} kcal</b> </p>
+      <button onClick={() => deleteFood(food.name)}>Delete</button>
+    </div>
+  );
+}
+export default FoodBox;
