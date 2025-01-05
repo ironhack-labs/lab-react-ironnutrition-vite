912489a83eacd12554e5957de9b09881fd6a829b
MMARTID <miguelmartindelgado1998@gmail.com>
2025-01-05 01:56:19 +0100

    iteration 5, add new item to aray

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,29 @@ import "./App.css";
 import { useState } from "react";
 import foodsJson from "./foods.json";
 import FoodBox from "./components/FoodBox";
-
+import AddFoodForm from "./components/AddFoodForm";
 function App() {
   const [food, setFood] = useState(foodsJson);
-  console.log("hi", food)
+  console.log(food);
+  const addFood = (newFood) => {
+    setFood([...food, newFood]);
+    console.log(food);
+  };
+
   const deleteFood = (foodName) => {
-    const updatedFood = food.filter((eachFood) => eachFood.name !== foodName)
-    setFood(updatedFood)
-  }
+    const updatedFood = food.filter((eachFood) => eachFood.name !== foodName);
+    setFood(updatedFood);
+  };
+
   return (
     <div className="App">
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      <div>
+        <AddFoodForm addFood={addFood} />
+      </div>
 
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
         {food.map((eachFood) => {
           return (
             <div key={eachFood.name + eachFood.image}>
@@ -29,7 +40,6 @@ function App() {
             </div>
           );
         })}
-
       </div>
     </div>
   );
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,98 @@
-// Your code here
+import { useState } from "react";
+function AddFoodForm({ addFood }) {
+   
+    const [newFood, setNewForm] = useState({
+        name: '',
+        image: '',
+        calories: '',
+        servings: ''
+    })
+    
+    const handleChange = e => {
+        setNewForm({
+            ...newFood,
+            [e.target.name] : e.target.value
+        })
+    }
+    console.log(newFood)
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        addFood(newFood); 
+       
+      };
+  return (
+    <>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          maxWidth: "300px",
+          margin: "0 auto",
+        }}
+      >
+        <input
+        onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="Food Name"
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <input
+        onChange={handleChange}
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <input
+        onChange={handleChange}
+          type="number"
+          name="calories"
+          placeholder="Calories"
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <input
+        onChange={handleChange}
+          type="number"
+          name="servings"
+          placeholder="Servings"
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          style={{
+            padding: "10px",
+            borderRadius: "5px",
+            border: "none",
+            backgroundColor: "#007BFF",
+            color: "#fff",
+            cursor: "pointer",
+          }}
+        >
+          Create
+        </button>
+      </form>
+    </>
+  );
+}
+export default AddFoodForm;
