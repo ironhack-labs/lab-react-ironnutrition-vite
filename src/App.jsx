8bf591f05dd06c18e580c01059cd07359ca5f02a
Juan Manuel <j-grafi@hotmail.com>
2025-01-16 18:04:48 +0100

    4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBoox/FoodBox";
+
 
 function App() {
+  const [foods, setFoods] =useState(foodsJson);
+
+  const deleteFood = (foodId) =>{
+    const filterFood = foods.filter((food)=> food.id !== foodId)
+    setFoods(filterFood)
+
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <div>
+      {foods.map((food)=>( <FoodBox deleteFood={deleteFood}  key={food.id} food={food}/>))}
+        
+      </div>
     </div>
   );
 }
