f5da4a9b904b317550af9b715ea623e2244a95a4
theresajvrensburg <theresa@isogramconsulting.com>
2025-01-17 16:21:40 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodList from "./components/FoodList";
 
 function App() {
+ 
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
+      <FoodList foodData={foodsJson}/>
     </div>
   );
 }
