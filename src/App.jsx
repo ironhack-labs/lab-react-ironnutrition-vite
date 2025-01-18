680c532fcab00a38d12006c4925f9386e2bf829f
Ricardo García <ricardogarciajarillo@gmail.com>
2025-01-18 08:50:09 +0100

    feat: iterations 1-6

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import FoodList from "./components/FoodList";
 
 
+
 function App() {
   return (
     <div className="App">
+    <h1>LAB React-Ironnutrition</h1>
       <FoodList />
     </div>
   );
